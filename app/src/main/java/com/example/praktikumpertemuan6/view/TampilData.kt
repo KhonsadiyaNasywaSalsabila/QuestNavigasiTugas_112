3e0962b9d04da65bd80c9fd4cb144ace141a4ffe
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-11-08 03:18:17 +0700

    Inisialisasi TampilData dengan layout Column utama

--- app/src/main/java/com/example/praktikumpertemuan6/view/TampilData.kt
+++ app/src/main/java/com/example/praktikumpertemuan6/view/TampilData.kt
@@ -1,2 +1,36 @@
 package com.example.praktikumpertemuan6.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TampilData(
+    modifier: Modifier = Modifier,
+    onNavigateToWelcome: () -> Unit,
+    onNavigateToForm: () -> Unit
+) {
+
+
+    Column(
+
+    ) {
+
+    }
+}
