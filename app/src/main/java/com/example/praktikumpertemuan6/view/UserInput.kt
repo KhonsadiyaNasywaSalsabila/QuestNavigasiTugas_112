4bc187a1eb443f6877d1a8df3a15b734f6d5b5d2
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-11-08 02:04:17 +0700

    mendefinisikan data statis untuk RadioButton

--- app/src/main/java/com/example/praktikumpertemuan6/view/UserInput.kt
+++ app/src/main/java/com/example/praktikumpertemuan6/view/UserInput.kt
@@ -50,6 +50,9 @@ fun FormDataDiri2(modifier: Modifier = Modifier) {
 
     var showDialog by remember { mutableStateOf(false) }
 
+    val gender: List<String> = listOf("Laki-laki", "Perempuan")
+    val statusPerkawinan: List<String> = listOf("Janda", "Lajang", "Duda")
+
     val purpleBackground = Color(0xFFE6D5F5)
     val purpleButton = Color(0xFF7C3AED)
     val purpleHeader = Color(0xFFB794F6)
