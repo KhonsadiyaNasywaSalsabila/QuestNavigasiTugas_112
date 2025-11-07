fb0be1162e31948fea34d2fba8418db89421d986
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-11-08 04:18:48 +0700

    Inisialisasi NavHost dengan enum AlurLayar dan startDestination

--- app/src/main/java/com/example/praktikumpertemuan6/Navigasi.kt
+++ app/src/main/java/com/example/praktikumpertemuan6/Navigasi.kt
@@ -1,2 +1,37 @@
 package com.example.praktikumpertemuan6
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.praktikumpertemuan6.view.FormDataDiri2
+import com.example.praktikumpertemuan6.view.TampilData
+import com.example.praktikumpertemuan6.view.WelcomeScreen
+
+enum class AlurLayar {
+    Welcome,
+    Form,
+    Data
+}
+
+
+@Composable
+fun AplikasiNavigasi(
+    navController: NavHostController = rememberNavController()
+) {
+    Scaffold { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = AlurLayar.Welcome.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+
+        }
+    }
+}
