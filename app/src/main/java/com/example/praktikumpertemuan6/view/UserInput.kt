924f3c96f3ff75d964c615f62ea261a97b92adfe
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-11-08 01:54:19 +0700

    Tambahkan 'remember' state untuk data yang akan ditampilkan di dialog

--- app/src/main/java/com/example/praktikumpertemuan6/view/UserInput.kt
+++ app/src/main/java/com/example/praktikumpertemuan6/view/UserInput.kt
@@ -43,6 +43,11 @@ fun FormDataDiri2(modifier: Modifier = Modifier) {
     var textJK by remember { mutableStateOf("") }
     var textStatus by remember { mutableStateOf("") }
 
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenis by remember { mutableStateOf("") }
+    var status by remember { mutableStateOf("") }
+
     val purpleBackground = Color(0xFFE6D5F5)
     val purpleButton = Color(0xFF7C3AED)
     val purpleHeader = Color(0xFFB794F6)
